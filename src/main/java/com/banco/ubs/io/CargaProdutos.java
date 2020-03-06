f0561696a5bf08a18c884c7b5ad6c4a3209a8734
vtrhp <vtrhp@hotmail.com>
2020-03-05 23:15:34 -0300

    Ajuste na leitura dos arquivos

--- src/main/java/com/banco/ubs/io/CargaProdutos.java
+++ src/main/java/com/banco/ubs/io/CargaProdutos.java
@@ -1,34 +1,55 @@
 package com.banco.ubs.io;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.io.InputStreamReader;
 
 import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
 
-
+import com.banco.ubs.entities.Estoque;
+import com.banco.ubs.service.impl.EstoqueServiceImpl;
 
 public class CargaProdutos {
 
 	public static void main(String[] args) throws IOException {
-	     JSONObject json = null;
+		JSONObject jsonObject;
+	     JSONParser parser = new JSONParser();
+	     Estoque estoque = new Estoque();
+	     EstoqueServiceImpl estoqueService;
+	     StringBuilder json = new StringBuilder();
 	        try {
-	            FileReader fileReader = new FileReader("/ubs/src/main/resources/arquivos/data_1.json");
-	            BufferedReader bufferedReader = new BufferedReader(fileReader);
-	            List<String> collect = bufferedReader.lines().collect(Collectors.toList());
-	            StringBuilder jsonTemp = new StringBuilder();
-	            for (String s : collect) {
-	                jsonTemp.append(s);
-	            }
-	            json = new JSONObject(jsonTemp.toString());
-	            System.out.println(json.toString());
-	            fileReader.close();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	            System.err.println("Não foi possível ler o arquivo json");
-	        }
+	        	estoqueService = new EstoqueServiceImpl();
+	        	File dir = new File("D:\\DESENVOLVIMENTO\\WORKSPACES\\workspace_banco_ubs\\ubs\\src\\main\\resources\\arquivos");
+	        	File[] files = dir.listFiles(filter);
+	        	for(int i = 0; i<files.length; i++) {
+	             BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(files[i])));
+	            
+	             String linha = br.readLine();
+
+	             while(linha != null) {
+	                 System.out.println(linha);
+	                 jsonObject = (JSONObject) parser.parse(linha.toString());
+	                 estoque.setProduto((String) jsonObject.get("product"));
+	                 estoque.setQuantidade(Integer.valueOf((String)jsonObject.get("quantity")));
+	                 estoque.setPreco(Double.valueOf((String) jsonObject.get("price")));
+	                 estoque.setTipo((String) jsonObject.get("type"));
+	                 estoque.setIndustria((String) jsonObject.get("industry"));
+	                 estoque.setOrigem((String) jsonObject.get("origin"));
+	                 estoqueService.persistir(estoque);
+	             }
+	        	}
+	             } catch (Exception e) {
+	            	 e.printStackTrace();
+				}
 	}
 
+	static FileFilter filter = new FileFilter() {
+		public boolean accept(File file) {
+			return file.getName().endsWith(".json");
+		}
+	};
 }
