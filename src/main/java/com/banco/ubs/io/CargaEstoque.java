f06ef8ffb831d2a77c21b6e0cbefc02f7d37ce43
vtrhp <vtrhp@hotmail.com>
2020-03-15 16:53:27 -0300

    Criado utils para ler o diretório de arquivos configurado no properties

--- src/main/java/com/banco/ubs/io/CargaEstoque.java
+++ src/main/java/com/banco/ubs/io/CargaEstoque.java
@@ -12,6 +12,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.EnumSet;
+import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -28,6 +29,7 @@ import org.springframework.stereotype.Component;
 
 import com.banco.ubs.entities.Estoque;
 import com.banco.ubs.service.EstoqueService;
+import com.banco.ubs.utils.Utils;
 
 @Component
 public class CargaEstoque {
@@ -113,7 +115,8 @@ public class CargaEstoque {
 	}
 	
 	private File[] lePath() {
-    	File dir = new File("D:\\DESENVOLVIMENTO\\WORKSPACES\\workspace_banco_ubs\\ubs\\src\\main\\resources\\arquivos");
+		Properties prop = Utils.getProp();
+    	File dir = new File(prop.getProperty("prop.dir"));
         File[] files = dir.listFiles(filter);
         return files;
 	}
